e16af30eca5c22c0a71ed8a65555c3e133b91bf2
ntalydevs <ntalydevs@gmail.com>
2025-09-10 23:21:05 +0300

    chore: initial commit

---
+++ components/setup-progress/ProgressBar.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const ProgressBar = () => {
+  return (
+    <View>
+      <Text className="text-red-500">ProgressBar</Text>
+    </View>
+  );
+};
+
+export default ProgressBar;
