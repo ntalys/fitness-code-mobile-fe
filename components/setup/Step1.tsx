9c330245254db742ab52ab8842e9f1b23c870d25
ntalydevs <ntalydevs@gmail.com>
2025-09-20 21:54:52 +0300

    feat: added functionality to close keyboard or overlays whenever the user tab somewhere else on the screen

--- components/setup/Step1.tsx
+++ components/setup/Step1.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { H4, Input, Label, Text, XStack, YStack } from "tamagui";
 import { CustomSelectOpt } from "../custom/CustomSelectOpt";
 
-import { Platform } from "react-native";
+import { Keyboard, Platform, Pressable } from "react-native";
 
 import PopoverCalendarIOS from "../custom/PopoverCalendarIOS";
 import PopoverCalendarAndroid from "../custom/PopoverCalendarAndroid";
@@ -16,73 +16,89 @@ const Step1 = () => {
   const [show, setShow] = useState(false);
   const [date, setDate] = useState<Date | null>(new Date());
 
+  const [value, onValueChange] = useState("");
+
   return (
-    <YStack>
-      <YStack alignItems="start" gap="$1">
-        <YStack mb={15}>
-          <H4 fontWeight={600}>Personal Information</H4>
-        </YStack>
-        <XStack alignItems="center" justifyContent="space-between" gap={10}>
-          <YStack flex={1}>
-            <Label width={90} htmlFor="fname">
-              First Name*
-            </Label>
-            <Input
-              id="fname"
-              defaultValue=""
-              keyboardAppearance="default"
-              keyboardType="default"
-              placeholder="First name"></Input>
+    <Pressable
+      onPress={() => Keyboard.dismiss()}
+      style={{ width: "100%" }}
+      android_disableSound={false} // optional for Android
+    >
+      <YStack>
+        <YStack alignItems="start" gap="$1">
+          <YStack mb={15}>
+            <H4 fontWeight={600}>Personal Information</H4>
           </YStack>
-          <YStack flex={1}>
-            <Label width={90} htmlFor="lname">
-              Last Name*
-            </Label>
-            <Input
-              id="lname"
-              defaultValue=""
-              keyboardAppearance="default"
-              keyboardType="default"
-              placeholder="Last name"
+          <XStack alignItems="center" justifyContent="space-between" gap={10}>
+            <YStack flex={1}>
+              <Label width={90} htmlFor="fname">
+                First Name*
+              </Label>
+              <Input
+                id="fname"
+                defaultValue=""
+                keyboardAppearance="default"
+                keyboardType="default"
+                placeholder="First name"></Input>
+            </YStack>
+            <YStack flex={1}>
+              <Label width={90} htmlFor="lname">
+                Last Name*
+              </Label>
+              <Input
+                id="lname"
+                defaultValue=""
+                keyboardAppearance="default"
+                keyboardType="default"
+                placeholder="Last name"
+              />
+            </YStack>
+          </XStack>
+
+          <Label width={90} htmlFor="gender">
+            Gender*
+          </Label>
+          <CustomSelectOpt
+            value={value}
+            onValueChange={onValueChange}
+            items={items}
+            labelTitle="Genders"
+            maxWidth={420}
+            placeholder="Select Gender"
+            onOpenChange={() => Keyboard.dismiss()}
+          />
+
+          <Label width={90} htmlFor="dateOfBirth">
+            Date of Birth*
+          </Label>
+          {Platform.OS === "ios" ? (
+            <PopoverCalendarIOS
+              setDate={setDate}
+              date={date}
+              labelTitle={date.toLocaleDateString()}
             />
-          </YStack>
-        </XStack>
+          ) : (
+            <PopoverCalendarAndroid
+              date={date}
+              show={show}
+              setDate={setDate}
+              setShow={setShow}
+            />
+          )}
 
-        <Label width={90} htmlFor="gender">
-          Gender*
-        </Label>
-        <CustomSelectOpt items={items} labelTitle="Genders" />
-
-        <Label width={90} htmlFor="dateOfBirth">
-          Date of Birth*
-        </Label>
-        {Platform.OS === "ios" ? (
-          <PopoverCalendarIOS
-            setDate={setDate}
-            date={date}
-            labelTitle={date.toLocaleDateString()}
+          <Label width={90} htmlFor="email">
+            Email*
+          </Label>
+          <Input
+            id="email"
+            defaultValue=""
+            keyboardAppearance="default"
+            keyboardType="email-address"
+            placeholder="name@example.com"
           />
-        ) : (
-          <PopoverCalendarAndroid
-            date={date}
-            show={show}
-            setDate={setDate}
-            setShow={setShow}
-          />
-        )}
-
-        <Label width={90} htmlFor="email">
-          Email*
-        </Label>
-        <Input
-          id="email"
-          defaultValue=""
-          keyboardAppearance="default"
-          keyboardType="email-address"
-          placeholder="name@example.com"
-        />
+        </YStack>
       </YStack>
-    </YStack>
+    </Pressable>
   );
 };
 
