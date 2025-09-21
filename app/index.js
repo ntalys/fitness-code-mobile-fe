8f7351e7b6f5b0a6b1fa819efc6ab6f2b4280f3d
ntalydevs <ntalydevs@gmail.com>
2025-09-21 15:01:37 +0300

    feat: added scroll view, fix: style issue

--- app/index.js
+++ app/index.js
@@ -13,7 +13,11 @@ import config from "../tamagui.config";
 import { useState } from "react";
 import { ArrowLeft, ArrowRight, Check } from "lucide-react-native";
 import { Step1, Step2, Step3, Step4, Step5 } from "../components/setup";
-import { KeyboardProvider } from "react-native-keyboard-controller";
+import {
+  KeyboardAvoidingView,
+  KeyboardProvider,
+} from "react-native-keyboard-controller";
+import { Platform, ScrollView } from "react-native";
 
 export default function Page() {
   const [progress, setProgress] = useState(20);
@@ -35,87 +39,100 @@ export default function Page() {
     <SafeAreaProvider>
       <TamaguiProvider config={config} defaultTheme={"light"}>
         <KeyboardProvider>
-          {/* Header */}
-          <XStack justifyContent="start">
-            <YStack justifyContent="center" alignItems="center">
+          <KeyboardAvoidingView
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+            style={{ flex: 1 }}>
+            {/* Header */}
+            <XStack
+              alignItems="center"
+              justifyContent="center"
+              px="$4"
+              pt={"$4"}>
+              <H3>Setup Your Profile</H3>
+
               {step > 1 && (
                 <Button
                   unstyled
-                  circular
+                  // circular
                   size="$6"
+                  pt={"$4"}
                   icon={ArrowLeft}
-                  justifyContent="center"
                   onPress={onProgressStepDecrease}
                   style={{
+                    position: "absolute",
+                    left: -4,
                     justifyContent: "center",
-                    alignItems: "center",
                     backgroundColor: "transparent",
                   }}
                 />
               )}
-            </YStack>
-
-            <YStack
-              px={"$6"}
-              py={"$3"}
-              justifyContent="center"
-              alignItems="center">
-              <H3>Setup Your Profile</H3>
-            </YStack>
-          </XStack>
-
-          {/* Progress bar */}
-          <YStack
-            style={{ justifyContent: "space-between", padding: 25, gap: 18 }}>
-            <XStack style={{ justifyContent: "space-between" }}>
-              <Text>{step}/5</Text>
-              <Text>{progress}%</Text>
             </XStack>
 
-            <YStack>
-              <XStack>
-                <Progress
-                  value={progress}
-                  max={100}
-                  style={{ backgroundColor: "white" }}>
-                  <Progress.Indicator
-                    animation="quick"
-                    style={{ backgroundColor: "hsla(51, 100%, 58%, 1)" }}
-                  />
-                </Progress>
-              </XStack>
-            </YStack>
+            <ScrollView
+              contentContainerStyle={{
+                flexGrow: 1,
+                paddingVertical: 0,
+                paddingBottom: 20,
+              }}
+              keyboardShouldPersistTaps="handled">
+              {/* Progress bar */}
+              <YStack
+                style={{
+                  justifyContent: "space-between",
+                  padding: 25,
+                  gap: 18,
+                }}>
+                <XStack style={{ justifyContent: "space-between" }}>
+                  <Text>{step}/5</Text>
+                  <Text>{progress}%</Text>
+                </XStack>
 
-            {/* Main content */}
-            <YStack backgroundColor="white" rounded={"$6"} p={"$4"}>
-              <YStack style={{ display: step === 1 ? "flex" : "none" }}>
-                <Step1 />
-              </YStack>
-              <YStack style={{ display: step === 2 ? "flex" : "none" }}>
-                <Step2 />
-              </YStack>
-              <YStack style={{ display: step === 3 ? "flex" : "none" }}>
-                <Step3 />
-              </YStack>
-              <YStack style={{ display: step === 4 ? "flex" : "none" }}>
-                <Step4 />
-              </YStack>
-              <YStack style={{ display: step === 5 ? "flex" : "none" }}>
-                <Step5 />
-              </YStack>
-            </YStack>
+                <YStack>
+                  <XStack>
+                    <Progress
+                      value={progress}
+                      max={100}
+                      style={{ backgroundColor: "white" }}>
+                      <Progress.Indicator
+                        animation="quick"
+                        style={{ backgroundColor: "hsla(51, 100%, 58%, 1)" }}
+                      />
+                    </Progress>
+                  </XStack>
+                </YStack>
 
-            {/* Step button */}
-            <XStack width={"$100"} justify={step < 5 ? "start" : "center"}>
-              <Button
-                size="$6"
-                iconAfter={step < 5 ? ArrowRight : Check}
-                onPress={onProgressStepIncrease}
-                theme={"accent"}>
-                {step < 5 ? "Continue" : "Complete Setup"}
-              </Button>
-            </XStack>
-          </YStack>
+                {/* Main content */}
+                <YStack backgroundColor="white" rounded={"$6"} p={"$4"}>
+                  <YStack style={{ display: step === 1 ? "flex" : "none" }}>
+                    <Step1 />
+                  </YStack>
+                  <YStack style={{ display: step === 2 ? "flex" : "none" }}>
+                    <Step2 />
+                  </YStack>
+                  <YStack style={{ display: step === 3 ? "flex" : "none" }}>
+                    <Step3 />
+                  </YStack>
+                  <YStack style={{ display: step === 4 ? "flex" : "none" }}>
+                    <Step4 />
+                  </YStack>
+                  <YStack style={{ display: step === 5 ? "flex" : "none" }}>
+                    <Step5 />
+                  </YStack>
+                </YStack>
+
+                {/* Step button */}
+                <XStack width={"$100"} justify={step < 5 ? "start" : "center"}>
+                  <Button
+                    size="$6"
+                    iconAfter={step < 5 ? ArrowRight : Check}
+                    onPress={onProgressStepIncrease}
+                    theme={"accent"}>
+                    {step < 5 ? "Continue" : "Complete Setup"}
+                  </Button>
+                </XStack>
+              </YStack>
+            </ScrollView>
+          </KeyboardAvoidingView>
         </KeyboardProvider>
       </TamaguiProvider>
     </SafeAreaProvider>
