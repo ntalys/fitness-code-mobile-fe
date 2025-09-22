dc41883996c0e4dace66ba3ef8838384d08a57c9
ntalydevs <ntalydevs@gmail.com>
2025-09-23 00:29:20 +0300

    chore: sample step5

--- components/setup/Step5.tsx
+++ components/setup/Step5.tsx
@@ -1,10 +1,36 @@
 import { View, Text } from "react-native";
 import React from "react";
 
-const Step5 = () => {
+const Step5 = ({
+  personalInfo,
+  physicalMeasurements,
+  fitnessGaol,
+  fitnessExp,
+}) => {
   return (
     <View>
-      <Text>Step5</Text>
+      <Text>First Name: {personalInfo.fname}</Text>
+      <Text>Last Name: {personalInfo.lname}</Text>
+      <Text>Gender: {personalInfo.gender}</Text>
+      <Text>Birthday: {personalInfo.birthday?.toLocaleDateString()}</Text>
+      <Text>Email: {personalInfo.email}</Text>
+
+      <Text>
+        Height: {physicalMeasurements.height.value}{" "}
+        {physicalMeasurements.height.unit}
+      </Text>
+      <Text>
+        Weight: {physicalMeasurements.weight.value}{" "}
+        {physicalMeasurements.weight.unit}
+      </Text>
+
+      <Text>
+        Fitness Goals:{" "}
+        {Array.isArray(fitnessGaol) ? fitnessGaol.join(", ") : fitnessGaol}
+      </Text>
+
+      <Text>Fitness Level: {fitnessExp.fitnessLevel}</Text>
+      <Text>Workout Frequency: {fitnessExp.workoutFrequency}</Text>
     </View>
   );
 };
