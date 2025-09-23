b772d14981b70907d29a2333d8a460d00107e196
ntalydevs <ntalydevs@gmail.com>
2025-09-24 00:28:46 +0300

    feat: implemented step5

--- components/setup/Step5.tsx
+++ components/setup/Step5.tsx
@@ -1,5 +1,5 @@
-import { View, Text } from "react-native";
 import React from "react";
+import { H6, Text, XStack, YStack } from "tamagui";
 
 const Step5 = ({
   personalInfo,
@@ -8,30 +8,143 @@ const Step5 = ({
   fitnessExp,
 }) => {
   return (
-    <View>
-      <Text>First Name: {personalInfo.fname}</Text>
-      <Text>Last Name: {personalInfo.lname}</Text>
-      <Text>Gender: {personalInfo.gender}</Text>
-      <Text>Birthday: {personalInfo.birthday?.toLocaleDateString()}</Text>
-      <Text>Email: {personalInfo.email}</Text>
+    <YStack>
+      <YStack alignItems="start" gap="$3">
+        <YStack>
+          <H6 fontWeight={400}>Review Your Information</H6>
+        </YStack>
 
-      <Text>
-        Height: {physicalMeasurements.height.value}{" "}
-        {physicalMeasurements.height.unit}
-      </Text>
-      <Text>
-        Weight: {physicalMeasurements.weight.value}{" "}
-        {physicalMeasurements.weight.unit}
-      </Text>
+        <YStack
+          bg={"$shadow1"}
+          borderTopRightRadius={"$4"}
+          borderTopLeftRadius={"$4"}
+          borderBottomLeftRadius={"$4"}
+          borderBottomRightRadius={"$4"}
+          p={12}>
+          <YStack flex={1} gap={10}>
+            <XStack>
+              <H6 fontWeight={300}>Personal Information</H6>
+            </XStack>
+            <XStack flex={1} gap={3}>
+              <Text>First Name:</Text>
+              <Text color={"$color10"}>{personalInfo.fname}</Text>
+            </XStack>
+            <XStack flex={1} gap={3}>
+              <Text>Last Name:</Text>
+              <Text color={"$color10"}>{personalInfo.lname}</Text>
+            </XStack>
+            <XStack flex={1} gap={3}>
+              <Text>Gender:</Text>
+              <Text color={"$color10"}>{personalInfo.gender}</Text>
+            </XStack>
+            <XStack flex={1} gap={3}>
+              <Text>Birthday:</Text>
+              <Text color={"$color10"}>{personalInfo.birthdays}</Text>
+            </XStack>
+            <XStack flex={1} gap={3}>
+              <Text>Email:</Text>
+              <Text color={"$color10"}>{personalInfo.email}</Text>
+            </XStack>
+          </YStack>
+        </YStack>
 
-      <Text>
-        Fitness Goals:{" "}
-        {Array.isArray(fitnessGaol) ? fitnessGaol.join(", ") : fitnessGaol}
-      </Text>
+        <YStack
+          bg={"$shadow1"}
+          borderTopRightRadius={"$4"}
+          borderTopLeftRadius={"$4"}
+          borderBottomLeftRadius={"$4"}
+          borderBottomRightRadius={"$4"}
+          p={12}>
+          <YStack flex={1} gap={10}>
+            <XStack>
+              <H6 fontWeight={300}>Physical Measurements</H6>
+            </XStack>
+            <XStack flex={1}>
+              <XStack gap={3}>
+                <Text>Height:</Text>
+                <Text color={"$color10"}>
+                  {physicalMeasurements.height.value}
+                </Text>
+                <Text color={"$color10"}>
+                  {physicalMeasurements.height.unit}
+                </Text>
+              </XStack>
+            </XStack>
+            <XStack flex={1}>
+              <XStack gap={3}>
+                <Text>Weight:</Text>
+                <Text color={"$color10"}>
+                  {physicalMeasurements.weight.value}
+                </Text>
+                <Text color={"$color10"}>
+                  {physicalMeasurements.weight.unit}
+                </Text>
+              </XStack>
+            </XStack>
+          </YStack>
+        </YStack>
 
-      <Text>Fitness Level: {fitnessExp.fitnessLevel}</Text>
-      <Text>Workout Frequency: {fitnessExp.workoutFrequency}</Text>
-    </View>
+        <YStack
+          bg={"$shadow1"}
+          borderTopRightRadius={"$4"}
+          borderTopLeftRadius={"$4"}
+          borderBottomLeftRadius={"$4"}
+          borderBottomRightRadius={"$4"}
+          p={12}>
+          <YStack flex={1} gap={10}>
+            <XStack>
+              <H6 fontWeight={300}>Fitness Goals</H6>
+            </XStack>
+            <XStack flex={1}>
+              <XStack gap={3} flexWrap="wrap" alignItems="center">
+                <Text>Fitness Goals:</Text>
+                {fitnessGaol.map((goal, idx) => (
+                  <XStack
+                    key={idx}
+                    borderTopRightRadius={"$4"}
+                    borderTopLeftRadius={"$4"}
+                    borderBottomLeftRadius={"$4"}
+                    borderBottomRightRadius={"$4"}
+                    bg="$color10"
+                    px={10}
+                    py={5}
+                    alignItems="center"
+                    justifyContent="center">
+                    <Text color="$color1">{goal}</Text>
+                  </XStack>
+                ))}
+              </XStack>
+            </XStack>
+          </YStack>
+        </YStack>
+
+        <YStack
+          bg={"$shadow1"}
+          borderTopRightRadius={"$4"}
+          borderTopLeftRadius={"$4"}
+          borderBottomLeftRadius={"$4"}
+          borderBottomRightRadius={"$4"}
+          p={12}>
+          <YStack flex={1} gap={10}>
+            <XStack>
+              <H6 fontWeight={300}>Physical Measurements</H6>
+            </XStack>
+            <XStack flex={1}>
+              <XStack gap={3}>
+                <Text>Fitness Level:</Text>
+                <Text color={"$color10"}>{fitnessExp.fitnessLevel}</Text>
+              </XStack>
+            </XStack>
+            <XStack flex={1}>
+              <XStack gap={3}>
+                <Text>Workout Frequency:</Text>
+                <Text color={"$color10"}>{fitnessExp.workoutFrequency}</Text>
+              </XStack>
+            </XStack>
+          </YStack>
+        </YStack>
+      </YStack>
+    </YStack>
   );
 };
 
