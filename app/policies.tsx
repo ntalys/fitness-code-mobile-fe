ab34a97c31c728f603c53745a30e41842d1302ac
ntalydevs <ntalydevs@gmail.com>
2025-10-05 10:34:24 +0300

    chore: sample policies screen

---
+++ app/policies.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const policies = () => {
+  return (
+    <View>
+      <Text>policies</Text>
+    </View>
+  );
+};
+
+export default policies;
