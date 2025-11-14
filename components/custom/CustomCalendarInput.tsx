1f92162760cf3217fd3a05dfc337510d0aca83b6
ntalydevs <ntalydevs@gmail.com>
2025-11-14 20:52:13 +0200

    fix: to support dark mode also

--- components/custom/CustomCalendarInput.tsx
+++ components/custom/CustomCalendarInput.tsx
@@ -16,20 +16,18 @@ const CustomCalendarInput = ({ children, date }) => {
         alignItems: "center",
         justifyContent: "space-between",
         flexWrap: "nowrap",
-        backgroundColor: "hsla(0, 16%, 94%, 1)",
+        backgroundColor: "$color2",
         width: "100%",
-        borderTopColor: "hsla(0, 15%, 77%, 1)",
-        borderRightColor: "hsla(0, 15%, 77%, 1)",
-        borderLeftColor: "hsla(0, 15%, 77%, 1)",
-        borderBottomColor: "hsla(0, 15%, 77%, 1)",
+        borderTopColor: "$color2",
+        borderRightColor: "$color2",
+        borderLeftColor: "$color2",
+        borderBottomColor: "$color2",
         maxWidth: 420,
         overflow: "hidden",
         borderRadius: 9,
         borderWidth: 1,
       }}>
-      <Text style={{ color: date ? "black" : "hsla(0, 15%, 50%, 1)" }}>
-        {date ? date : "dd/mm/yyyy"}
-      </Text>
+      <Text>{date ? date : "dd/mm/yyyy"}</Text>
       {children}
     </XStack>
   );
