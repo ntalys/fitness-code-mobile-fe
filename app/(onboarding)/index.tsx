30b59498be25c57b78ba8a0b15f1b1ce0c16c86c
ntalydevs <ntalydevs@gmail.com>
2025-11-16 01:30:27 +0200

    feat: onboarding screen

---
+++ app/(onboarding)/index.tsx
@@ -0,0 +1,111 @@
+import { useRouter } from "expo-router";
+import React, { useState } from "react";
+import { XStack, YStack, Button } from "tamagui";
+import OnBoardingSlider from "../../components/on-boarding/OnBoardingSlider";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useColorScheme } from "react-native";
+import DotIndicator from "../../components/dot-indicator/DotIndicator";
+import Welcome from "../../assets/on-boarding/welcome.svg";
+import ProgressOverview from "../../assets/on-boarding/progressOverview.svg";
+import PlanWorkout from "../../assets/on-boarding/planWorkout.svg";
+import BuildForYou from "../../assets/on-boarding/coach.svg";
+
+import { ArrowRight } from "lucide-react-native";
+const welcome = () => {
+  const colorScheme = useColorScheme(); // "light" | "dark"
+  const router = useRouter();
+
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const slides = [
+    {
+      svg: <Welcome />,
+      title: "Welcome to Fitness Code",
+      description:
+        "Your personal fitness companion. Track workouts, monitor progress, and achieve your fitness goals with ease",
+    },
+    {
+      svg: <ProgressOverview />,
+      title: "Track Your Progress",
+      description:
+        "Monitor your fitness journey with detailed analytics, workout history, and personalized insights.",
+    },
+    {
+      svg: <PlanWorkout />,
+      title: "Plan Your Workouts",
+      description:
+        "Schedule workouts, set reminders, and stay consistent with our smart calendar system.",
+    },
+    {
+      svg: <BuildForYou />,
+      title: "Built for You",
+      description:
+        "Created by fitness enthusiasts for fitness enthusiasts. Join thousands of users transforming their lives.",
+    },
+  ];
+
+  console.log("colorScheme: ", colorScheme);
+
+  return (
+    <SafeAreaView
+      style={{
+        backgroundColor:
+          colorScheme === "dark"
+            ? "hsla(0, 15%, 1%, 1)"
+            : "hsla(0, 18%, 99%, 1)",
+      }}>
+      <YStack width={"100%"} height={"100%"} justify="center">
+        <XStack position="absolute" t={10} r={24}>
+          <Button unstyled onPress={() => router.push("(auth)")}>
+            skip
+          </Button>
+        </XStack>
+        <YStack justify="center" gap={40}>
+          <XStack justify="center">
+            <OnBoardingSlider
+              svg={slides[currentSlide].svg}
+              title={slides[currentSlide].title}
+              description={slides[currentSlide].description}
+            />
+          </XStack>
+          <YStack justify="center">
+            <XStack justify="center" gap={5}>
+              {slides.map((_, index) => (
+                <DotIndicator
+                  key={index}
+                  onPress={() => setCurrentSlide(index)}
+                  active={index === currentSlide}
+                />
+              ))}
+            </XStack>
+          </YStack>
+          <XStack justify="center">
+            <Button
+              width={360}
+              theme="accent"
+              color={"$color"}
+              onPress={() => {
+                setCurrentSlide((prev) => {
+                  if (prev < slides.length - 1) {
+                    return prev + 1; // move to next slide
+                  }
+
+                  // last slide → navigate
+                  router.push("(auth)");
+                  return prev; // keep same value
+                });
+              }}
+              iconAfter={
+                <ArrowRight
+                  color={colorScheme === "dark" ? "white" : "black"}
+                />
+              }>
+              {currentSlide !== slides.length - 1 ? "Next" : "Get Started"}
+            </Button>
+          </XStack>
+        </YStack>
+      </YStack>
+    </SafeAreaView>
+  );
+};
+
+export default welcome;
