657d05e9bf7c2fd600c9cc21ab05b5b6131d775f
ntalydevs <ntalydevs@gmail.com>
2025-11-23 19:28:39 +0200

    feat: root component for custom toasts

---
+++ components/toast/ToastInSteroids.tsx
@@ -0,0 +1,28 @@
+import { useColorScheme } from "react-native";
+import React from "react";
+import Toast, { BaseToastProps } from "react-native-toast-message";
+import ErrorToast from "./ErrorToast";
+import SuccessToast from "./SuccessToast";
+import InfoToast from "./InfoToast";
+import uuid from "react-native-uuid";
+
+const config = (colorScheme) => ({
+  EToast: ({ ...props }: BaseToastProps) => (
+    <ErrorToast {...props} colorScheme={colorScheme} key={uuid.v4()} />
+  ),
+  SToast: ({ ...props }: BaseToastProps) => (
+    <SuccessToast {...props} colorScheme={colorScheme} key={uuid.v4()} />
+  ),
+
+  IToast: ({ ...props }: BaseToastProps) => (
+    <InfoToast {...props} colorScheme={colorScheme} key={uuid.v4()} />
+  ),
+});
+
+const ToastInSteroids2 = () => {
+  const colorScheme = useColorScheme();
+
+  return <Toast config={config(colorScheme)} />;
+};
+
+export default ToastInSteroids2;
