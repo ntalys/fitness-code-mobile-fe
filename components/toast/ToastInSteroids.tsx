110e045663fdc374e587fb7aa486badecaf477ab
ntalydevs <ntalydevs@gmail.com>
2025-11-28 20:59:11 +0200

    fix: added the necessary components to be used

--- components/toast/ToastInSteroids.tsx
+++ components/toast/ToastInSteroids.tsx
@@ -1,21 +1,39 @@
-import { useColorScheme } from "react-native";
+import { ColorSchemeName, useColorScheme } from "react-native";
 import React from "react";
 import Toast, { BaseToastProps } from "react-native-toast-message";
-import ErrorToast from "./ErrorToast";
-import SuccessToast from "./SuccessToast";
-import InfoToast from "./InfoToast";
 import uuid from "react-native-uuid";
+import BeastToast from "./BeastToast";
 
-const config = (colorScheme) => ({
-  EToast: ({ ...props }: BaseToastProps) => (
-    <ErrorToast {...props} colorScheme={colorScheme} key={uuid.v4()} />
+const config = (colorScheme: ColorSchemeName) => ({
+  BeastSuccessToast: ({ ...props }: BaseToastProps) => (
+    <BeastToast
+      {...props}
+      colorScheme={colorScheme}
+      statusColor="green"
+      collapsable={false}
+      expanded={true}
+      key={uuid.v4()}
+    />
   ),
-  SToast: ({ ...props }: BaseToastProps) => (
-    <SuccessToast {...props} colorScheme={colorScheme} key={uuid.v4()} />
+  BeastErrorToast: ({ ...props }: BaseToastProps) => (
+    <BeastToast
+      {...props}
+      colorScheme={colorScheme}
+      statusColor="red"
+      collapsable={false}
+      expanded={true}
+      key={uuid.v4()}
+    />
   ),
-
-  IToast: ({ ...props }: BaseToastProps) => (
-    <InfoToast {...props} colorScheme={colorScheme} key={uuid.v4()} />
+  BeastInfoToast: ({ ...props }: BaseToastProps) => (
+    <BeastToast
+      {...props}
+      colorScheme={colorScheme}
+      statusColor="blue"
+      collapsable={false}
+      expanded={true}
+      key={uuid.v4()}
+    />
   ),
 });
 
