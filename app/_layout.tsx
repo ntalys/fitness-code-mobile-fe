4f26c4f1ba78702dff9ebedd3c8034e7e3a26a0c
ntalydevs <ntalydevs@gmail.com>
2025-12-15 17:43:21 +0200

    feat: use auth provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,21 +6,24 @@ import { TamaguiProvider, Theme } from "tamagui";
 
 import config from "../tamagui.config";
 import { useColorScheme } from "react-native";
+import { AuthProvider } from "../context/AuthContext";
 
 const _layout = () => {
   const colorScheme = useColorScheme(); // "light" | "dark"
 
   return (
     <>
-      <TamaguiProvider config={config}>
-        <Theme name={colorScheme === "dark" ? "dark" : "light"}>
-          <Stack screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="(auth)" />
-            <Stack.Screen name="(tabs)" />
-            <Stack.Screen name="+not-found" />
-          </Stack>
-        </Theme>
-      </TamaguiProvider>
+      <AuthProvider>
+        <TamaguiProvider config={config}>
+          <Theme name={colorScheme === "dark" ? "dark" : "light"}>
+            <Stack screenOptions={{ headerShown: false }}>
+              <Stack.Screen name="(auth)" />
+              <Stack.Screen name="(tabs)" />
+              <Stack.Screen name="+not-found" />
+            </Stack>
+          </Theme>
+        </TamaguiProvider>
+      </AuthProvider>
     </>
   );
 };
